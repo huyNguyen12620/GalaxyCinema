b10b9b0de28c526d4f39ae18ebeb6f14c6ef2387
huyNguyen12620 <huynguyen1262000@gmail.com>
2023-03-02 22:39:59 +0700

    deploy1

--- src/HomeTemplates/HomeTemplates.jsx
+++ src/HomeTemplates/HomeTemplates.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import { Outlet } from "react-router-dom";
 import Header from '../components/Header/Header'
-import Menu from '../components/Menu/Menu';
 import Footer from './../components/Footer/Footer';
-
+import Menu from '../components/Menu/Menu';
 export default function HomeTemplates() {
   return (
     <>
