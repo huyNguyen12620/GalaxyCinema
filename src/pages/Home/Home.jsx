f0f4abbc63c9090cd77e7309cfc50bd8bfea800f
huyNguyen12620 <huynguyen1262000@gmail.com>
2023-03-02 22:48:54 +0700

    deploy3

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
+import Carousel_Home from 'components/Carousel/Carousel_Home';
+import MenuFilm from 'components/MenuFilm/MenuFilm';
 import React from 'react';
-import Carousel_Home from '../../components/Carousel/Carousel_Home';
-import Menu from '../../components/Menu/Menu';
-import MenuFilm from '../../components/MenuFilm/MenuFilm';
+
 import About from '../About/About';
 
 
